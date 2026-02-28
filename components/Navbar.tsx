1dcb7369ba7810fee3b5b93dcce1d9d8af95d769
rashmin <rashminxperience@gmail.com>
2026-02-28 18:36:40 +0545

    Phase One Complete

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,8 @@
 "use client";
-import { cn } from "@/lib/utils";
+
+import Link from "next/link";
+import Image from "next/image";
+import { usePathname } from "next/navigation";
 import {
   SignedIn,
   SignedOut,
@@ -7,29 +10,31 @@ import {
   UserButton,
   useUser,
 } from "@clerk/nextjs";
-import Image from "next/image";
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 const navItems = [
   { label: "Library", href: "/" },
   { label: "Add New", href: "/books/new" },
+  { label: "Pricing", href: "/subscriptions" },
 ];
 
 const Navbar = () => {
   const pathName = usePathname();
   const { user } = useUser();
+
   return (
-    <header className="w-full fixed z-50 bg-('--bg-primary')">
+    <header className="w-full fixed z-50 bg-(--bg-primary)">
       <div className="wrapper navbar-height py-4 flex justify-between items-center">
         <Link href="/" className="flex gap-0.5 items-center">
-          <Image src="/assets/logo.png" alt="Readly" width={42} height={26} />
+          <Image src="/assets/logo.png" alt="Bookfied" width={42} height={26} />
           <span className="logo-text">Readly</span>
         </Link>
+
         <nav className="w-fit flex gap-7.5 items-center">
           {navItems.map(({ label, href }) => {
             const isActive =
               pathName === href || (href !== "/" && pathName.startsWith(href));
+
             return (
               <Link
                 href={href}
@@ -43,6 +48,7 @@ const Navbar = () => {
               </Link>
             );
           })}
+
           <div className="flex gap-7.5 items-center">
             <SignedOut>
               <SignInButton mode="modal" />
