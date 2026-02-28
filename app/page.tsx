db97c0d70ccbef1c557f721612c3c55a8052c973
rashmin <rashminxperience@gmail.com>
2026-02-28 13:45:28 +0545

    Completed Books Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
+import BookCard from "@/components/BookCard";
 import HeroSection from "@/components/hero-section";
+import { sampleBooks } from "@/lib/constants";
 
 export default function Page() {
   return (
-    <main>
+    <main className="wrapper container">
       <HeroSection />
+      <div className="library-books-grid">
+        {sampleBooks.map((book) => (
+          <BookCard
+            key={book._id}
+            title={book.title}
+            author={book.author}
+            coverURL={book.coverURL}
+            slug={book.slug}
+          />
+        ))}
+      </div>
     </main>
   );
 }
